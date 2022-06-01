22b663a01a082d4f45b93c3ed75130ed5b4a08af
SheikhRashed <devrashed06@gmail.com>
2022-06-01 23:42:30 +0600

    update: homepage header created

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,37 @@
 "use strict"
 
+// mobile Nav Sidebar
+
+const toggleBtn = document.querySelector(".toggleBtn")
+
+toggleBtn.addEventListener("click", (e) => {
+	e.preventDefault()
+	toggleBtn.classList.toggle("active")
+
+	document.querySelector(".mobile-menu").classList.toggle("active")
+})
+
+// Sticky navbar on scroll
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+	myFunction()
+}
+
+// Get the navbar
+var navbar = document.querySelector(".header")
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.pageYOffset >= 120) {
+		navbar.classList.add("sticky")
+	} else {
+		navbar.classList.remove("sticky")
+	}
+}
+
 var swiper = new Swiper(".bannerSlide", {
 	loop: true,
 	// autoplay: {
